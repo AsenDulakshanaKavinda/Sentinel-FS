6757b26165b706c8568ce0edf0ec806bed8bbf38
AsenDulakshanaKavinda <s22010362@ousl.lk>
2026-04-20 05:23:58 +0530

    add tool mod

--- file_sys_tools/src/lib.rs
+++ file_sys_tools/src/lib.rs
@@ -1,4 +1,5 @@
 use pyo3::prelude::*;
+mod tools;
 
 /// A Python module implemented in Rust. The name of this module must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
---
+++ file_sys_tools/src/tools/mod.rs
@@ -0,0 +1,3 @@
+pub mod file_context;
+pub mod index_dir;
+pub mod search_code;
